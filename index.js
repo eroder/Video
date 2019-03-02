977dc5050132529e93a67f1077bbb51f5333d994
eroder <eronides@gmail.com>
2019-03-02 11:34:05 -0300

    Add manual text

---
+++ index.js
@@ -0,0 +1,29 @@
+function start() {
+	
+	const content = {
+	 	searchTerm: "Banana",
+		prefix: "3 Dicas Sobre",
+	 	sentences: [
+	   	{
+	   		text: "Tem mais de 100 tipos de bananas",
+	   		keywords: ["tipos de bananas"],
+	   		images: ["..."]
+	   	},
+	   	{
+	   		text: "banana é rica em potacio",
+	   		keywords: ["potacio"],
+	   		images: ["..."]
+	   	},
+	    	{
+	   		text: "banana ajuda na caganeira",
+	   		keywords: ["caganeira"],
+	   		images: ["..."]
+	    	}
+	  	]
+	}
+
+	console.log(content)
+
+}
+
+start()
