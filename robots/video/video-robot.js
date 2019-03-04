a0dd19b05963a292e6b0500b107819ed646e5fd8
eroder <eronides@gmail.com>
2019-03-04 06:30:10 -0300

    linked video-robot.js to ae template

---
+++ robots/video/video-robot.js
@@ -0,0 +1,10 @@
+
+var content = {
+	 	texto1: "Tem mais de 100 tipos de bananas",
+	 	foto1: "robots/video/AE/placeHolders/foto1.png",
+	 	texto2: "banana é rica em potassio",
+	 	foto2: "robots/video/AE/placeHolders/foto2.png",
+	 	texto3: "banana ajuda na caganeira",
+	 	foto3: "robots/video/AE/placeHolders/foto3.png",
+
+}
